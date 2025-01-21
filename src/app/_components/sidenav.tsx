442bb4f3978d654fb96de36a8e3e17d11ce69818
Austin <aconnor731@gmail.com>
2025-01-21 16:32:49 -0500

    fix links side panel

--- src/app/_components/sidenav.tsx
+++ src/app/_components/sidenav.tsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import cn from 'classnames';
-import { useHamburger } from '@/app/context/HamburgerContext'; // Import context
-import { IconButton } from '@mui/material';
-import { SideNavItem } from '../ui/types';
-import Link from 'next/link';
-import HomeIcon from '@mui/icons-material/Home'; 
+'use client'
+
+import React from 'react'
+import cn from 'classnames'
+import { useHamburger } from '@/app/context/HamburgerContext' // Import context
+import { IconButton } from '@mui/material'
+import { SideNavItem } from '../ui/types'
+import Link from 'next/link'
+import HomeIcon from '@mui/icons-material/Home'
+import DescriptionIcon from '@mui/icons-material/Description'
 
 interface SidePanelProps {
-  onSelectItem?: (item: string) => void;
+  onSelectItem?: (item: string) => void
 }
 
 const sideNavItems: SideNavItem[] = [
   { label: 'Home', element: <HomeIcon />, href: '/' },
+  { label: 'Blog', element: <DescriptionIcon />, href: '/posts' },
   // Add other items as needed
-];
+]
 
 const SidePanel: React.FC<SidePanelProps> = (props: SidePanelProps) => {
-  const { isOpen } = useHamburger(); // Use the isOpen state from context
+  const { isOpen } = useHamburger() // Use the isOpen state from context
 
   return (
     <div
@@ -24,39 +28,40 @@ const SidePanel: React.FC<SidePanelProps> = (props: SidePanelProps) => {
         'h-screen shadow-lg transition-all duration-500 ease-in-out fixed left-0 z-40 bg-white dark:bg-gray-800', // Tailwind transition and fixed positioning
 
         {
-          'w-auto min-w-[200px]': isOpen,  // Large width when opened
+          'w-auto min-w-[200px]': isOpen, // Large width when opened
           'w-0 overflow-hidden': !isOpen, // Ensure it's completely hidden when closed
-        },
+        }
       )}
       style={{ top: '6.28rem' }} // Adjust margin to be below the navbar
     >
       <ul className="flex flex-col">
         {sideNavItems.map((item, index) => (
-          <li
-            key={index}
-            className="rounded-md cursor-pointer hover:bg-blue-100 transition-colors flex items-center space-x-2 p-2"
-          >
-            <Link href={item.href} passHref>
-              <IconButton key={index} aria-label={item.label} className='pl-4 text-gray-800 dark:text-gray-300'>
+          <Link href={item.href} passHref>
+            <li
+              key={index}
+              className="rounded-md cursor-pointer hover:bg-blue-100 transition-colors flex items-center space-x-2 p-2"
+            >
+              <IconButton
+                key={index}
+                aria-label={item.label}
+                className="pl-4 text-gray-800 dark:text-gray-300"
+              >
                 {item.element}
               </IconButton>
-            </Link>
-            <span
-              className={cn(
-                'pt-1 transition-all duration-500 ease-in-out',
-                {
+              <span
+                className={cn('pt-1 transition-all duration-500 ease-in-out', {
                   'opacity-100 translate-x-0': isOpen,
                   'opacity-0 translate-x-[-100%]': !isOpen, // Only transition the label
-                }
-              )}
-            >
-              {item.label}
-            </span>
-          </li>
+                })}
+              >
+                {item.label}
+              </span>
+            </li>
+          </Link>
         ))}
       </ul>
     </div>
-  );
-};
+  )
+}
 
-export default SidePanel;
+export default SidePanel
