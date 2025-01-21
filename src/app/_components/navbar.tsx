e04cbfc82e35936066673085aa4556f071fc06a6
Austin <aconnor731@gmail.com>
2025-01-21 16:42:37 -0500

    overflow into navbar fixed

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,18 +1,18 @@
-'use client';
+'use client'
 
-import React from 'react';
-import RotatingButton from './rotating-button/rotating-button';
-import HamburgerMenu from './hamburger';
+import React from 'react'
+import RotatingButton from './rotating-button/rotating-button'
+import HamburgerMenu from './hamburger'
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="dark:bg-slate-900 dark:text-slate-400 fixed top-0 left-0 z-50 w-full shadow-lg">
+    <nav className="navbar dark:bg-slate-900 dark:text-slate-400 fixed top-0 left-0 z-50 w-full shadow-lg">
       <div className="flex justify-between items-center px-4">
         <HamburgerMenu />
         <RotatingButton />
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -1,18 +1,23 @@
-import { Metadata } from "next";
+import { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Posts Section',
   description: 'Explore our posts',
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-      <div className="flex h-screen">
-        <div className="flex-1 p-4 mt-8 overflow-y-auto">{children}</div>
+    <div className="flex h-screen">
+      <div
+        className="flex-1 p-4 overflow-y-auto"
+        style={{ marginTop: 'var(--navbar-height)' }}
+      >
+        {children}
       </div>
-  );
+    </div>
+  )
 }
