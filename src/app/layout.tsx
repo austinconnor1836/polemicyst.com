07d4986cd405a1adba3e40428cd1b1e53965ffec
Austin <aconnor731@gmail.com>
2025-01-22 15:14:35 -0500

    fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,26 @@
-import "@/app/ui/global.css";
-import React from 'react';
-import LocalFont from "next/font/local";
-import { Metadata } from "next";
-import { Provider } from "react-redux";
-import { Analytics } from "@/app/_components/analytics";
-import { HamburgerProvider, useHamburger } from '@/app/context/HamburgerContext';
-import { ThemeSwitcher } from "./_components/theme-switcher";
-import { inter } from "./ui/fonts";
-import ClientLayout from './client-layout';
-import Navbar from './_components/navbar';
-import StoreProvider from "./StoreProvider";
-import HamburgerMenu from "./_components/hamburger";
-import { SideNavItem } from "./ui/types";
-import SidePanel from "./_components/sidenav";
-import SharedLayout from "./SharedLayout";
+import '@/app/ui/global.css'
+import React from 'react'
+import LocalFont from 'next/font/local'
+import { Metadata } from 'next'
+import { Analytics } from '@/app/_components/analytics'
+import { HamburgerProvider, useHamburger } from '@/app/context/HamburgerContext'
+import { inter } from './ui/fonts'
+import StoreProvider from './StoreProvider'
+import SharedLayout from './SharedLayout'
 
 export const metadata: Metadata = {
   title: {
-    default: "polemicyst.com",
-    template: "%s | polemicyst.com",
+    default: 'polemicyst.com',
+    template: '%s | polemicyst.com',
   },
-  description: "Founder of Polemicyst.",
+  description: 'Founder of Polemicyst.',
   openGraph: {
-    title: "polemicyst.com",
-    description:
-      "Founder of Tyromaniac.",
-    url: "https://polemicyst.com",
-    siteName: "polemicyst.com",
-    locale: "en-US",
-    type: "website",
+    title: 'polemicyst.com',
+    description: 'Founder of Tyromaniac.',
+    url: 'https://polemicyst.com',
+    siteName: 'polemicyst.com',
+    locale: 'en-US',
+    type: 'website',
   },
   robots: {
     index: true,
@@ -36,29 +28,28 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
     },
   },
   twitter: {
-    title: "Polemicyst",
-    card: "summary_large_image",
+    title: 'Polemicyst',
+    card: 'summary_large_image',
   },
   icons: {
-    icon: "/favicon/favicon.png",
+    icon: '/favicon/favicon.png',
   },
-};
+}
 
 const calSans = LocalFont({
-  src: "../../public/fonts/CalSans-SemiBold.ttf",
-  variable: "--font-calsans",
-});
+  src: '../../public/fonts/CalSans-SemiBold.ttf',
+  variable: '--font-calsans',
+})
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-
   return (
-    <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+    <html lang="en" className={[inter.variable, calSans.variable].join(' ')}>
       <head>
         <Analytics />
         <link
@@ -109,5 +100,5 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         </StoreProvider>
       </body>
     </html>
-  );
+  )
 }
